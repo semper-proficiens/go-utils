b133d1d864c4d71459a06447738f9d43f8fb4596
nubesFilius <eperezlarosa@outlook.com>
2024-09-22 12:46:41 -0400

    add_time_helpers

--- system/time/time.go
+++ system/time/time.go
@@ -27,3 +27,39 @@ func TimeUntilNextRun(location string, scheduledHour, scheduledMin int) (time.Du
 		return time.Until(dateTimeScheduled), nil
 	}
 }
+
+// GetFirstDayOfThisMonth returns the 1st day of this month in time.Time, just format to the desired format.
+//
+// e.g. log.Println("First day of this month:", firstDayOfCurrentMonth.Format("2006-01-02"))
+//
+// e.g. log.Println("First day of the month in Unix:", firstDayOfCurrentMonth.Unix() )
+func GetFirstDayOfThisMonth() time.Time {
+	now := time.Now()
+	firstDayOfCurrentMonth := time.Date(now.Year(), now.Month(), 1, 0, 0, 0, 0, now.Location())
+	//log.Println("First day of this month:", firstDayOfCurrentMonth.Format("2006-01-02"))
+	return firstDayOfCurrentMonth
+}
+
+// GetLastDayOfLastMonth returns the 1st day of this month in time.Time, just format to the desired format.
+//
+// e.g. log.Println("First day of this month:", lastDayOfLastMonth.Format("2006-01-02"))
+//
+// e.g. log.Println("First day of the month in Unix:", lastDayOfLastMonth.Unix() )
+func GetLastDayOfLastMonth() time.Time {
+	firstDayOfCurrentMonth := GetFirstDayOfThisMonth()
+	lastDayOfLastMonth := firstDayOfCurrentMonth.AddDate(0, 0, -1)
+	//log.Println("Last day of last month:", lastDayOfLastMonth.Format("2006-01-02"))
+	return lastDayOfLastMonth
+}
+
+// GetFirstDayOfLastMonth returns the 1st day of this month in time.Time, just format to the desired format.
+//
+// e.g. log.Println("First day of this month:", firstDayOfLastMonth.Format("2006-01-02"))
+//
+// e.g. log.Println("First day of the month in Unix:", firstDayOfLastMonth.Unix() )
+func GetFirstDayOfLastMonth() time.Time {
+	lastDayOfLastMonth := GetLastDayOfLastMonth()
+	firstDayOfLastMonth := time.Date(lastDayOfLastMonth.Year(), lastDayOfLastMonth.Month(), 1, 0, 0, 0, 0, lastDayOfLastMonth.Location())
+	//log.Println("First day of last month:", firstDayOfLastMonth.Format("2006-01-02"))
+	return firstDayOfLastMonth
+}
---
+++ system/time/time_test.go
@@ -0,0 +1,15 @@
+package time
+
+import "testing"
+
+func TestGetFirstDayOfThisMonth(t *testing.T) {
+	GetFirstDayOfThisMonth()
+}
+
+func TestGetLastDayOfThisMonth(t *testing.T) {
+	GetLastDayOfLastMonth()
+}
+
+func TestGetFirstDayOfLastMonth(t *testing.T) {
+	GetFirstDayOfLastMonth()
+}
